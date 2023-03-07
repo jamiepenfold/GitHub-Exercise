9759fe4d6e10600e45a5c42b42d72a2f9dbef658
Jamie Penfold <jamiepenfold3@gmail.com>
2023-03-07 13:55:51 +0200

    Initial Commit

---
+++ GitHub Exercise/GitHub_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_ExerciseApp.swift
+//  GitHub Exercise
+//
+//  Created by jamie penfold on 2023/03/07.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
